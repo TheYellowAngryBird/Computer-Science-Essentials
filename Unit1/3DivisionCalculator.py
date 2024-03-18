ed361289557f17d5683cd497b62690da0e693573
TheYellowAngryBird <162594525+TheYellowAngryBird@users.noreply.github.com>
2024-03-18 08:33:12 -0400

    Create 3DivisionCalculator.py

---
+++ Unit1/3DivisionCalculator.py
@@ -0,0 +1,8 @@
+print("Welcome to the Division Calculator")
+print("This Calculator divides two numbers given by the user.")
+print("Enter a number")
+x=int(input())
+print("Enter another number")
+y=int(input())
+print("Your answer is:")
+print(x/y)
